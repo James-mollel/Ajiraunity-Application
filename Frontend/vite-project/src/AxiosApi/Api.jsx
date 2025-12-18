2756eaa33e4644839c1dd189a7a37d88284a0b56
james <jamesmollel004@gmail.com>
2025-12-18 19:28:13 +0300

    Update api ->  react

--- Frontend/vite-project/src/AxiosApi/Api.jsx
+++ Frontend/vite-project/src/AxiosApi/Api.jsx
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_BASE_URL = "https://ajiraunity-backend.onrender.com"
 
 const api = axios.create({
     baseURL: API_BASE_URL,
